b38758e7464fe3bf484457e63c3e79130d466af2
Oleksii <aar97@ukr.net>
2019-02-04 17:43:40 +0200

    Initialized spring boot

---
+++ src/main/java/INT20H/task/TaskApplication.java
@@ -0,0 +1,14 @@
+package INT20H.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskApplication.class, args);
+	}
+
+}
+
