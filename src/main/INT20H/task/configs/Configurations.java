9bbe37668df2209d26db099eacb340bad26705e2
Oleksii <aar97@ukr.net>
2019-02-04 19:41:27 +0200

    Added album controller and flickr interface

---
+++ src/main/INT20H/task/configs/Configurations.java
@@ -0,0 +1,7 @@
+package INT20H.task.configs;
+
+public class Configurations {
+    public static final String tag = "";
+    public static final String albumId = "";
+    public static final int imagesPerPage = 50;
+}
--- src/main/INT20H/task/controllers/AlbumController.java
+++ src/main/INT20H/task/controllers/AlbumController.java
@@ -1,7 +1,25 @@
 package INT20H.task.controllers;
 
+import INT20H.task.configs.Configurations;
+import INT20H.task.services.interfaces.FlickrService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/api")
 public class AlbumController {
+    private final FlickrService flickrService;
+
+    public AlbumController(FlickrService flickrService) {
+        this.flickrService = flickrService;
+    }
+
+    @GetMapping("/getAllImages")
+    public List<byte[]> getAllImages(@RequestParam(name = "page", defaultValue = "0", required = false) int page){
+        return flickrService.getImagesFromAlbumOrByTag(Configurations.tag, Configurations.albumId, page);
+    }
 }
