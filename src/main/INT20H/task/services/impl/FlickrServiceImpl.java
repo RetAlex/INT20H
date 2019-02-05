f62a146512dc1c847c7d77f47e8e48922e4caa82
NMaslukov <mikita.malykov@upholding.biz>
2019-02-05 19:59:34 +0200

    fix

--- src/main/INT20H/task/services/impl/FlickrServiceImpl.java
+++ src/main/INT20H/task/services/impl/FlickrServiceImpl.java
@@ -1,9 +1,11 @@
 package INT20H.task.services.impl;
 
 import INT20H.task.services._interfaces.FlickrService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class FlickrServiceImpl implements FlickrService {
     @Override
     public List<String> getImagesFromAlbumOrByTag(String albumId, String tag, int page) {
