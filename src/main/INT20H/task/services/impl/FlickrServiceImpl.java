5ccdf6ab02fb35ef6d51d316afff5aa81f0fd79b
NMaslukov <mikita.malykov@upholding.biz>
2019-02-05 19:47:29 +0200

    service impl

---
+++ src/main/INT20H/task/services/impl/FlickrServiceImpl.java
@@ -0,0 +1,12 @@
+package INT20H.task.services.impl;
+
+import INT20H.task.services._interfaces.FlickrService;
+
+import java.util.List;
+
+public class FlickrServiceImpl implements FlickrService {
+    @Override
+    public List<String> getImagesFromAlbumOrByTag(String albumId, String tag, int page) {
+        return null;
+    }
+}
