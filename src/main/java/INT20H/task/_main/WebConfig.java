b1b2b9c5eb099ebdc55fb68a78bcf640934dc42c
NMaslukov <mikita.malykov@upholding.biz>
2019-02-06 22:59:19 +0200

    cors disabled

--- src/main/java/INT20H/task/_main/WebConfig.java
+++ src/main/java/INT20H/task/_main/WebConfig.java
@@ -1,6 +1,7 @@
 package INT20H.task._main;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,5 +10,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
     }
 }
