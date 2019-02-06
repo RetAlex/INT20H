5adc322359a58d11cc7a52bd0a7d0afee077624c
NMaslukov <mikita.malykov@upholding.biz>
2019-02-06 22:54:42 +0200

    FIIIIIXXX

--- src/main/java/INT20H/task/_main/TaskApplication.java
+++ src/main/java/INT20H/task/_main/TaskApplication.java
@@ -2,8 +2,10 @@ package INT20H.task._main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = "INT20H.task")
+@EnableScheduling
 public class TaskApplication {
 
 	public static void main(String[] args) {
